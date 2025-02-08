43b18c73bfeb59d3dbb403e5e4fe0fd407d67a03
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-02-07 23:58:10 -0800

    Made changes to confirm Guestbook.

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 import  express from 'express';
 
 const app = express();
+const PORT = 3000;
+const signed = [];
 
 app.use(express.static('public'));
-
-const PORT = 3000;
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
      res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
+app.get('/admin/signed', (req, res) => {
+     res.send(signed);
+});
 
 app.listen(PORT, () => {
      console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+app.post('/submit-guest', (req, res) => {
+     // Fill in the form data
+     signed.push(req.body);
+
+     //Test operations:
+     console.log(req.body);
+
+     // Respond to the user with a confirmation page
+     res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+
 });
